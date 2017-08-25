d6db24d1f76bb1db2c696271b68752f0bf57e774
Mona Lisa <phiral246@gmail.com>
2017-08-25 18:36:25 +0530

    incident view modified

--- app/controllers/incidentsController.js
+++ app/controllers/incidentsController.js
@@ -1,18 +1,24 @@
 ﻿var incidentsController = riskManagementSystem.controller("incidentsController", ["$scope", "AppService", "rmsService", '$location', '$window', '$http', function($scope, AppService, rmsService, $location, $window, $http) {
     $scope.loggedInUser = rmsService.loggedInUser;
     $scope.token = localStorage.getItem('rmsAuthToken');
+    $scope.thisView = "incidents";
+    $scope.search = "";
     if (angular.isUndefined($scope.loggedInUser)) {
         $location.path("/")
     }
-    $scope.getData = function() {
+    $scope.getData = function(params) {
         var req = {
-            url: 'https://108296e7.ngrok.io/rmsrest/s/incident/incidents',
+            url: 'https://108296e7.ngrok.io//rmsrest/s/search-incident',
             method: "GET",
-            headers: { 'X-AUTH-TOKEN': $scope.token },
+            headers: {
+                'X-AUTH-TOKEN': $scope.token
+
+                // 'filters': params
+            },
         }
         AppService.ShowLoader();
-        var loginPromise = $http(req);
-        loginPromise.then(function(response) {
+        var getIncident = $http(req);
+        getIncident.then(function(response) {
             $scope.data = response.data;
             AppService.HideLoader();
 
@@ -21,5 +27,75 @@
             AppService.HideLoader();
         })
     }
-    $scope.getData();
+    $scope.getData('');
+
+    $scope.advancedSearch = function() {
+        var filters = [{
+                "field": "id",
+                "operator": "EQ",
+                "value": $scope.IncidentId
+
+            },
+            {
+                "field": "personInjured",
+                "operator": "EQ",
+                "value": $scope.InjuredPerson
+
+            },
+            {
+                "field": "organizationCode ",
+                "operator": "EQ",
+                "value": $scope.Organisation
+
+            },
+            {
+                "field": "departmentCode ",
+                "operator": "EQ",
+                "value": $scope.Dep
+
+            },
+            {
+                "field": "locationCode ",
+                "operator": "EQ",
+                "value": $scope.Loc
+
+            },
+
+        ]
+        $scope.getData(filters);
+    }
+    $scope.changePage = function() {
+
+    }
+    $scope.getEntries = function() {
+        alert($scope.entryCount);
+        var req = {
+            url: 'https://108296e7.ngrok.io//rmsrest/s/search-incident',
+            method: "GET",
+            headers: {
+                'X-AUTH-TOKEN': $scope.token,
+
+                'paging': {
+                    "currentPage": 0,
+                    "pageSize": $scope.entryCount
+                },
+
+            },
+        }
+        AppService.ShowLoader();
+        var getIncident = $http(req);
+        getIncident.then(function(response) {
+            $scope.data = response.data;
+            AppService.HideLoader();
+
+
+        }, function(error) {
+            AppService.HideLoader();
+        })
+    }
+    $scope.entry = [
+        { value: 10 },
+        { value: 25 },
+        { value: 50 }
+    ]
 }])
