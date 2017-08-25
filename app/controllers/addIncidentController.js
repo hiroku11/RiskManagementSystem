ef23172d5c8956b3b645698b1b8c83ef15426988
unknown <m.kumar2@PRO-LP084.prowareness.com>
2017-08-25 23:36:57 +0530

    UI for add incident all views

--- app/controllers/addIncidentController.js
+++ app/controllers/addIncidentController.js
@@ -4,8 +4,40 @@ var addIncidentController = riskManagementSystem.controller("addIncidentControll
     $scope.tab="1";
     $scope.authorizedUser=rmsService.decryptToken();
     $scope.loggedInUser = rmsService.getLoggedInUser();
-    // angular.element(function(){
-    //     $('#datetimepicker1').datetimepicker();
-    // })
+    
+    $scope.changeTab=function(tab){
+        $scope.tab=tab;
+        $(".content")[0].scrollTop=0;
+    }
+    $scope.getIncidentLocations=function(){
+        var req={
+            method:"GET",
+            url:"https://108296e7.ngrok.io/rmsrest/s/table-maintenance/accident-location/accident-locations",
+            headers:{
+                'X-AUTH-TOKEN':$scope.token 
+            }
+        }
+        $http(req).then(function(response){
+            //getting the incident locations from backend
+        })
+    }
 
+    //below function is not required as we are able ro get the location details in Locations api itself
+    // $scope.getIncidentLocationsDetails=function(){
+    //     var req={
+    //         method:"GET",
+    //         url:"https://108296e7.ngrok.io/rmsrest/s/table-maintenance/accident-location-detail/accident-locations-details",
+    //         headers:{
+    //             'X-AUTH-TOKEN':$scope.token 
+    //         }
+    //     }
+    //     $http(req).then(function(response){
+    //         debugger
+
+    //     })
+    // }
+    ///rmsrest/s/table-maintenance/accident- type/accident-types
+
+    $scope.getIncidentLocations();
+    //$scope.getIncidentLocationsDetails();
 }])
