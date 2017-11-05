2601baac06d1dbbf363bdbbfe20c1723436196af
Manish Kumar <manishjanky@gmail.com>
2017-11-05 13:40:55 +0530

    dashboard

--- app/controllers/dashboardController.js
+++ app/controllers/dashboardController.js
@@ -7,23 +7,41 @@
     //$scope.role = $scope.loggedInUser.roles[0];
     $scope.thisView = "dashboard";
     $scope.logOutUser = rmsService.logOutUser;
-    $scope.lookUp = function() {
+    // $scope.lookUp = function() {
+    //     var req = {
+    //         url: 'https://b2897cdb.ngrok.io/rmsrest/s/user-lookup',
+    //         method: "GET",
+    //         headers: { 'X-AUTH-TOKEN': $scope.token },
+    //     }
+    //     AppService.ShowLoader();
+    //     var loginPromise = $http(req);
+    //     loginPromise.then(function(response) {
+    //         $scope.data = response.data;
+    //         AppService.HideLoader();
+
+    //         $location.path("/dashboard")
+    //     }, function(error) {
+    //         AppService.HideLoader();
+    //     })
+    // }
+
+    $scope.getIncidentReportCount = function() {
         var req = {
-            url: 'https://b2897cdb.ngrok.io/rmsrest/s/user-lookup',
+            url: 'https://b2897cdb.ngrok.io/rmsrest/s/admin/admin-dashboard-hdr-stat',
             method: "GET",
             headers: { 'X-AUTH-TOKEN': $scope.token },
         }
         AppService.ShowLoader();
         var loginPromise = $http(req);
         loginPromise.then(function(response) {
-            $scope.data = response.data;
+            $scope.incidentReportCount = response.data;
             AppService.HideLoader();
-
-            $location.path("/dashboard")
         }, function(error) {
             AppService.HideLoader();
         })
     }
+
+    
     $scope.logOut = function() {
         AppService.ShowLoader();
         localStorage.removeItem("rmsAuthToken");
@@ -31,8 +49,8 @@
         $location.path("/login");
     }
     $scope.changeMenu = function() {
-
             $scope.thisView = $event.target.id;
-        }
-        // $scope.lookUp();
+    }
+    
+    $scope.getIncidentReportCount();
 }])
